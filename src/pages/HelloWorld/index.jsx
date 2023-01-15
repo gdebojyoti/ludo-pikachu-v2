59b1627d7f4db0b8765f8b069d207143ee5cf176
Debojyoti Ghosh <gdebojyoti@users.noreply.github.com>
2023-01-15 18:24:01 +0530

    Testing styled button with Linaria.

--- src/pages/HelloWorld/index.jsx
+++ src/pages/HelloWorld/index.jsx
@@ -1,13 +1,15 @@
 import React, { useState } from "react"
 
-import { bgStyle } from './style'
+import { bgStyle, StyledButton } from './style'
 
 const HelloWorld = () => {
   const [counter, updateCounter] = useState(0)
   return (
     <div className={bgStyle}>
       <div>I got in</div>
-      <button onClick={() => updateCounter(counter + 1)}>{counter ? `Clicked ${counter} time${counter === 1 ? '' : 's'}` : 'Click me!'}</button>
+      <StyledButton count={counter} onClick={() => updateCounter(counter + 1)}>
+        {counter ? `Clicked ${counter} time${counter === 1 ? '' : 's'}` : 'Click me!'}
+      </StyledButton>
     </div>
   )
 }
